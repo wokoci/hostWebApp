b4afffd7e540ea456455b69712bde0a17860a782
Jeff <wokoci@gmail.com>
2025-01-22 16:07:00 +0000

    Create setup.sh
    add setup bash script

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Switch to the root user to gain full administrative privileges
+sudo su
+
+# Update all installed packages to their latest versions
+yum update -y
+
+# Install Apache HTTP Server
+yum install -y httpd
+
+# Change the current working directory to the Apache web root
+cd /var/www/html
+
+# Install Git
+yum install git -y
+
+# Clone the project GitHub repository to the current directory
+git clone ?????????? git url here
+
+# Copy all files, including hidden ones, from the cloned repository to the Apache web root
+cp -R host-a-static-website-on-aws/. /var/www/html/
+
+# Remove the cloned repository directory to clean up unnecessary files
+rm -rf host-a-static-website-on-aws
+
+# Enable the Apache HTTP Server to start automatically at system boot
+systemctl enable httpd 
+
+# Start the Apache HTTP Server to serve web content
+systemctl start httpd
